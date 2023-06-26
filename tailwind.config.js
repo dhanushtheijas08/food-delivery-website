7f03bcba88b95f6fa46cf34c108c4ce67dc1ec43
“dhanushtheijas08” <21cs033@kpriet.ac.in>
2023-06-26 17:58:53 +0530

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orangeLight: "#fdf2e9",
+        orangeDark: "#e67e22",
+        textLigth: "#555",
+        textDark: "#333",
+        grayLight: "#ddd",
+      },
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+    },
+    fontWeight: {
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+    },
+  },
+  plugins: [],
+};
