04fb0af90e036f600f58c6a4dbe1df4f4bbaa103
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-06-28 11:22:42 +0530

    add meals section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import HeaderPage from "./pages/Header/HeaderPage.jsx";
 import HeroSection from "./pages/HeroSection/HeroSection.jsx";
+import SideBar from "./sample/SideBar.jsx";
 function App() {
   return (
     <div className="App font-rubik">
-      <HeaderPage className="" />
-      <HeroSection />
+      {/* <HeaderPage className="" />
+      <HeroSection /> */}
+      <SideBar />
     </div>
   );
 }
