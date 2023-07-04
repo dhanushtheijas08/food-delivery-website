ba864721c9f686477271860f8a5fb3d6d817e905
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-07-04 18:10:49 +0530

    added footer

--- src/pages/Contact/ContactCard.jsx
+++ src/pages/Contact/ContactCard.jsx
@@ -1,11 +1,12 @@
 import SubHeading from "../../components/SubHeading.jsx";
 import Button from "../../components/Button.jsx";
 import "./contact.css";
+
 function ContactCard() {
   return (
     <div className="max-w-7xl bg-[#e67e22] grid grid-cols-1 md:grid-cols-3 rounded-2xl text-[#45260a] shadow-2xl">
       <div className="mx-auto px-5 md:px-10 py-6 md:py-14 col-span-1 md:col-span-2">
-        <SubHeading classname="text-[#45260a] text-xl md:text-2xl lg:text-4xl font-semibold">
+        <SubHeading classname="text-[#45260a] text-xl md:text-3xl lg:text-5xl font-semibold">
           Get your first meal free!
         </SubHeading>
 
@@ -14,7 +15,7 @@ function ContactCard() {
           well today. You can cancel or pause anytime. And the first meal is on
           us!
         </p>
-        <div className="max-w-2xl mt-4 md:mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <form className="max-w-2xl mt-4 md:mt-8 grid grid-cols-1 lg:grid-cols-2 gap-4">
           <div className="flex flex-col">
             <label htmlFor="fname" className="text-lg font-medium">
               First Name
@@ -51,7 +52,7 @@ function ContactCard() {
           <button className="text-xl bg-[#45260a] text-white h-fit self-end py-2 rounded-lg md:mt-2 lg:mt-0">
             Try Now
           </button>
-        </div>
+        </form>
       </div>
       <div className="orange-filter rounded-r-2xl hidden md:block"></div>
     </div>
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <footer className="grid grid-cols-5 justify-between"></footer>;
+}
+
+export default Footer;
