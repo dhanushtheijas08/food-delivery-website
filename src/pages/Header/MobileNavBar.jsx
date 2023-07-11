f3c88ca1369a3a118dd63927afe3bd936bc4994e
dhanushtheijas08 <21cs033@kpriet.ac.in>
2023-07-11 09:08:22 +0530

    modified:   src/pages/Header/MobileNavBar.jsx

--- src/pages/Header/MobileNavBar.jsx
+++ src/pages/Header/MobileNavBar.jsx
@@ -1,10 +1,11 @@
 function MobileNavBar() {
-  return ReactDOM.createPortal(
-    <div>
-      <div className="w-full h-screen bg-slate-200 inset-0"></div>
-      <div className="w-full h-screen bg-red-200 inset-40">sdasd</div>
-    </div>,
-    document.querySelector(".model-container")
-  );
+  return <div></div>;
+  // return ReactDOM.createPortal(
+  //   <div>
+  //     <div className="w-full h-screen bg-slate-200 inset-0"></div>
+  //     <div className="w-full h-screen bg-red-200 inset-40">sdasd</div>
+  //   </div>,
+  //   document.querySelector(".model-container")
+  // );
 }
 export default MobileNavBar;
